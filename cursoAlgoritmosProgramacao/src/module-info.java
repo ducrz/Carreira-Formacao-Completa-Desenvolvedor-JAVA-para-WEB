fc79f5008576e0c89fbf00b08597aca24570ca67
Eduardo Monteiro <ducatolico@gmail.com>
2021-04-27 17:47:29 -0300

    Unidade 3

---
+++ cursoAlgoritmosProgramacao/src/module-info.java
@@ -0,0 +1,4 @@
+module cursoAlgoritmosProgramacao {
+    requires java.desktop;
+
+}
---
+++ cursoAlgoritmosProgramacao/src/unidade2/HelloWorld.java
@@ -0,0 +1,12 @@
+package unidade2;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Teste");
+		JOptionPane.showMessageDialog(null,"Teste");
+	}
+
+}
