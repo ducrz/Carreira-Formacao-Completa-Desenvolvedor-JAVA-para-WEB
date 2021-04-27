7d36d0dcf6d1925fb232b10bfd6e015755def5ba
Eduardo Monteiro <ducatolico@gmail.com>
2021-04-27 16:59:34 -0300

    Delete 3.java

--- 3.java
+++
@@ -1,8 +0,0 @@
-Scanner teclado = new Scanner(System.in);
-
-System.out.println("Digite o seu Número");
-
-double numero = teclado.nextDouble();
-double dobro= numero *2;
-
-System.out.println("O dobro do número é "+dobro);
